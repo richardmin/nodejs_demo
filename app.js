506ed305f9e2d51f49fc7bb3239ae5854e490d9f
Richard Min <richardmin97@gmail.com>
2016-05-14 12:40:37 -0700

    handlebars templates added.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var expressHbs = require('express3-handlebars');
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+
+app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
     res.send("Hello World");
@@ -12,6 +16,15 @@ app.get('/api_endpoint', function(req, res) {
 
 app.get('/user/:userID', function(req, res) {
 	res.send("<h1>Hello, " + req.params.userID + "<\/h1>");
+});
+
+app.get('/templates', function(req, res){
+	res.render('index');
+});
+
+app.get('/simple', function(req, res){
+  var data = {name: 'Gorilla'};
+  res.render('simple', data);
 });
 
 app.use('/static', express.static('static'));
